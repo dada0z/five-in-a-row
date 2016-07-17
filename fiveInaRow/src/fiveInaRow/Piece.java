4def1e2192dd3e23a5cf93d339d042c46072899f
dada0z <dada0z@163.com>
2016-07-17 17:49:41 +0800

    Separate Coordinate class from Piece class

--- fiveInaRow/src/fiveInaRow/Piece.java
+++ fiveInaRow/src/fiveInaRow/Piece.java
@@ -2,15 +2,13 @@
 
 import java.awt.Color;
 
-public class Piece {
+public class Piece extends Coordinate {
 	public static final int DIAMETER = 30;
-	private int x;
-	private int y;
 	private Color color = null;
 
 	public Piece(int x, int y, Color color) {
-		this.x = x;
-		this.y = y;
+		this.setX(x);
+		this.setY(y);
 		this.color = color;
 	}
 
@@ -18,23 +16,7 @@ public class Piece {
 		return color;
 	}
 
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
 	public void setColor(Color color) {
 		this.color = color;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public void setY(int y) {
-		this.y = y;
 	}
 }
